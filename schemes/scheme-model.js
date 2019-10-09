9259a051c0b8b2b454b1aa339a803b29cb434cd7
danRolon <daniel.rolon@yahoo.com>
2019-10-09 14:15:18 -0500

    mvp and strectch complete, needs better error handling

---
+++ schemes/scheme-model.js
@@ -0,0 +1,77 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove,
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first()
+    .then(scheme=>{
+      if(!scheme){
+        return null;
+      } else {
+        return scheme;
+      }
+    });
+}
+
+function findSteps(id){
+  return db('steps')
+    .join('schemes', 'schemes.id', 'steps.scheme_id')
+    .where( 'schemes.id', id)
+    .orderBy('steps.step_number')
+    .select('steps.id','schemes.scheme_name','steps.step_number','steps.instructions')
+}
+
+function add(scheme) {
+  return db('schemes')
+    .insert(scheme)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function addStep(step, id){
+  let stepData = step;
+  stepData.scheme_id = Number(id);
+  return db('steps')
+    .insert(stepData)
+    .then(ids=>{
+      return ids;
+    });
+}
+
+function update(changes, id) {
+  return db('schemes')
+    .where({ id })
+    .update(changes)
+    .then( ()=>{
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  let scheme = findById(id);
+  return db('schemes')
+    .where('id', id)
+    .del()
+    .then(res=>{
+      if(res){
+        return scheme;
+      } else{
+        return null;
+      }
+    });
+}
